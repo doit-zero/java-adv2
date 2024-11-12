a2b2f41228b50fd35d59037639a8a3d77f059ab2
doit-zero <131241458+doit-zero@users.noreply.github.com>
2024-11-12 21:14:15 +0900

    구성하는 역할과 사용하는 역할의 구분

--- src/webservice/MemberServerMain.java
+++ src/webservice/MemberServerMain.java
@@ -2,7 +2,11 @@ package webservice;
 
 import io.member.MemberRepository;
 import io.member.impl.FileMemberRepository;
+import was.httpserver.HttpServer;
+import was.httpserver.HttpSessionManager;
+import was.httpserver.ServletManager;
 import was.httpserver.servlet.annotation.AnnotationServletV2;
+import was.httpserver.servlet.annotation.AnnotationServletV3;
 
 import java.util.List;
 
@@ -12,7 +16,15 @@ public class MemberServerMain {
     public static void main(String[] args) {
         MemberRepository memberRepository = new FileMemberRepository();
         MemberController memberController = new MemberController(memberRepository);
-        List<MemberController> memberController1 = List.of(memberController);
+        List<Object> memberControllers = List.of(memberController);
 
+        ServletManager servletManager = new ServletManager();
+        AnnotationServletV3 annotationServletV3 = new AnnotationServletV3(memberControllers);
+        servletManager.setDefaultServlet(annotationServletV3);
+        HttpSessionManager httpSessionManager = new HttpSessionManager();
+
+        HttpServer httpServer = new HttpServer(PORT,httpSessionManager,servletManager);
+
+        httpServer.start();
     }
 }
